d4e80201f8e11c2a9f95a63c4aa2f9a552a96ffc
Ivan Pinduisaca <ivanpindui99@gmail.com>
2022-06-02 18:01:24 -0500

    Agregar mas secciones

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -56,15 +56,15 @@ export const Home = () => {
 
     const seccionTemplate = (seccion) => {
         return (
-            <div className="product-item text-black-alpha-90 p-2">
+            <div className="product-item text-white-alpha-90 p-2">
                 <div className="product-item-content">
                     <div className="mb-3">
-                        <img src={`src/images/seccions/${seccion.image}`} onError={(e) => e.target.src = 'https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png'} alt={seccion.nombreSeccion} className="product-image w-30rem h-23rem" />
+                        <img src={`src/images/seccions/${seccion.image}`} onError={(e) => e.target.src = 'https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png'} alt={seccion.nombreSeccion} className="product-image w-11 h-23rem border-round-xl" />
                     </div>
                     <div>
                         <h4 className="mb-1">{seccion.descripcion}</h4>
                         <div className="car-buttons mt-5">
-                            <Button label='Ver todos los productos' icon="pi pi-eye" className="p-button-secondary p-button-rounded mr-2" />
+                            <Button label='Ver todos los productos' icon="pi pi-eye" className="bg-orange-100 p-button-rounded" />
                         </div>
                     </div>
                 </div>
@@ -76,11 +76,11 @@ export const Home = () => {
         <div>
             <header>
                 <div className="card">
-                    <div className="flex card-container blue-container overflow-hidden">
-                        <div className="flex-none flex align-items-center justify-content-center m-2 px-5 border-round">
-                            <img src="src/images/LogoAguila.jpg" alt="Logo Multicomercio" className='border-round-xl w-20rem h-6rem cursor-pointer' onClick={logo} />
+                    <div className="flex card-container overflow-hidden">
+                        <div className="flex flex-column align-items-center justify-content-center m-2 px-5 border-round">
+                            <img src="src/images/LogoAguila.jpg" alt="Logo Multicomercio" className='border-round-xl w-20rem h-6rem cursor-pointer' onClick={logo} title='Multicomercio'  />
                         </div>
-                        <div className="flex-grow-1 flex align-items-center justify-content-start m-2 mt-3 px-5 border-round">
+                        <div className="flex-grow-1 flex align-items-center justify-content-start px-5 border-round">
                             <div className="card">
                                 <Button label='Productos' className="p-button-text text-black-alpha-90 text-xl text-center mx-5" icon={<FaShoppingBag className='text-2xl mr-1' />} />
                                 <Button label='Precios' className="p-button-text text-black-alpha-90 text-xl text-center mx-5" icon={<FaDollarSign className='text-2xl mr-1' />} />
@@ -88,7 +88,7 @@ export const Home = () => {
                                 <Button label='Nosotros' className="p-button-text text-black-alpha-90 text-xl text-center mx-5" icon={<BsInfoSquareFill className='text-2xl mr-1' />} />
                             </div>
                         </div>
-                        <div className="flex-none flex align-items-center justify-content-start m-2 mt-3 px-5 border-round">
+                        <div className="flex-none flex align-items-center justify-content-start px-5 border-round">
                             <Button label='Pedidos' className="p-button-text text-black-alpha-90 text-xl text-center mr-2" icon={<ImCart className='text-2xl mr-1' />} />
                             <Button label='Iniciar Sesión' onClick={login}
                                 className="bg-green-300 text-black-alpha-90 text-xl text-center mr-8" />
@@ -100,15 +100,13 @@ export const Home = () => {
 
             <article>
                 <Card className='m-3 bg-green-100 h-auto'>
-                    <Button label='Multicomercio' className="p-button-text text-black-alpha-90 text-xl text-center mr-2" icon='pi pi-home text-2xl' />
-
                     <article>
                         <div className="grid grid-nogutter surface-0 text-800 bg-green-100">
                             <div className="col-12 md:col-6 p-6 text-center md:text-left flex align-items-start ">
                                 <section>
-                                    <span className="block text-6xl text-black-alpha-90 font-bold mb-1">Ferreteria, Bazar, Papeleria, Tecnología y productos Descartables.</span>
+                                    <span className="block text-6xl text-black-alpha-90 font-bold mb-3">Ferreteria, Bazar, Papeleria, Tecnología y productos Descartables.</span>
                                     <div className="text-5xl text-indigo-700 font-bold mb-3">Encuentra todos los productos que estás buscando.</div>
-                                    <p className="text-3xl mt-0 mb-4 text-pink-600 line-height-3 mt-5">Hacemos envios de todos nuestros productos a nivel nacional e internacional.</p>
+                                    <p className="text-3xl mt-0 mb-4 text-pink-600 line-height-3 mt-3">Hacemos envios de todos nuestros productos a nivel nacional e internacional.</p>
                                 </section>
                             </div>
                             <div className="col-12 md:col-6 overflow-hidden">
@@ -120,8 +118,8 @@ export const Home = () => {
                             </div>
                         </div>
                         <div>
-                            <div className="grid grid-nogutter surface-0 text-800 bg-green-100">
-                                <div className="col-12 md:col-6 overflow-hidden mt-5 m-auto">
+                            <div className="grid grid-nogutter surface-0 text-800 mt-5 border-round">
+                                <div className="col-12 md:col-6 overflow-hidden m-auto">
                                     <div className="carousel-demo">
                                         <div className="card">
                                             <Carousel value={seccions} numVisible={1} numScroll={1} responsiveOptions={responsiveOptions} className="custom-carousel" circular
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -16,32 +16,32 @@ export const Login = () => {
     return (
         <div>
             <Button label="Regresar" icon="pi pi-arrow-left" onClick={regresar}
-            className="w-full mt-3 ml-3 p-button-rounded p-button-success w-8rem" />
+                className="w-full mt-3 ml-3 p-button-rounded p-button-success w-8rem" />
             <div className="flex align-items-center justify-content-center card-container h-screen">
                 <div className="surface-card p-4 shadow-2 border-round lg:w-4">
                     <div className="text-center mb-5">
-                        <img src="src/images/userLogin.jpg" alt="hyper" height={75} className="mb-3" />
-                        <div className="text-900 text-3xl font-medium mb-3">Welcome Back</div>
-                        <span className="text-600 font-medium line-height-3">Don't have an account?</span>
-                        <a className="font-medium no-underline ml-2 text-blue-500 cursor-pointer">Create today!</a>
+                        <img src="src/images/userLogin.png" alt="hyper" height={75} className="mb-3" />
+                        <div className="text-900 text-3xl font-medium mb-3">Bienvenido</div>
+                        <span className="text-600 font-medium line-height-3">No tienes una cuenta?</span>
+                        <a className="font-medium no-underline ml-2 text-900 cursor-pointer">Crear cuenta!</a>
                     </div>
 
                     <div>
-                        <label htmlFor="email" className="block text-900 font-medium mb-2">Email</label>
+                        <label htmlFor="email" className="block text-900 font-medium mb-2">Correo</label>
                         <InputText id="email" type="text" className="w-full mb-3" />
 
-                        <label htmlFor="password" className="block text-900 font-medium mb-2">Password</label>
+                        <label htmlFor="password" className="block text-900 font-medium mb-2">Contraseña</label>
                         <InputText id="password" type="password" className="w-full mb-3" />
 
                         <div className="flex align-items-center justify-content-between mb-6">
-                            <div className="flex align-items-center">
+                            <div className="flex align-items-center text-900">
                                 <Checkbox id="rememberme" onChange={e => setChecked(e.checked)} checked={checked} className="mr-2" />
-                                <label htmlFor="rememberme">Remember me</label>
+                                <label htmlFor="rememberme">Recordar mis credenciales</label>
                             </div>
-                            <a className="font-medium no-underline ml-2 text-blue-500 text-right cursor-pointer">Forgot your password?</a>
+                            <a className="font-medium no-underline ml-2 text-right cursor-pointer text-900">Olvide mi contraseña</a>
                         </div>
                         <div className="flex align-items-center justify-content-center">
-                            <Button label="Iniciar sesion" icon="pi pi-user" className="w-full p-button-rounded p-button-success w-full" />
+                            <Button label="Iniciar sesion" icon="pi pi-user" className="w-30rem p-button-rounded p-button-success p-3" />
                         </div>
                     </div>
                 </div>
